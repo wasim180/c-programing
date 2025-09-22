1dd16f2d7ea3eac56460c4d3ff7d465d796c9fc6
wasim180 <wa4641319@gmail.com>
2025-09-22 15:26:25 +0530

    Update assignment03 Q5.c
    update code

--- WASIM/Assignment 03/assignment03 Q5.c
+++ WASIM/Assignment 03/assignment03 Q5.c
@@ -1,6 +1,31 @@
 #include<stdio.h>
 int main(){
-  int mathematics,physics,chemistry;
+  int mathematics,physics,chemistry,total_marks,total_mark;
+
+  printf("Enter the obtained marks in Mathematics: ");
+  scanf("%d",&mathematics);
+
+  printf("Enter the obtained marks in Physics: ");
+  scanf("%d",&physics);
+
+  printf("Enter the obtained marks in chemistry:");
+  scanf("%d",&chemistry);
+
+  total_marks=(mathematics+physics+chemistry);
+  printf("Total marks=%d\n",total_marks);
+
+  total_mark=(mathematics+physics);
+  printf(" total mark(math+physics)=%d\n",total_mark);
+
+  if( total_marks>=190 || mathematics&physics>=140){
+    printf("this candidate is eligible.");
+  }
+  else{
+    printf("this candidate is not eligible.");
+  }
+
+
+
 
 
 
